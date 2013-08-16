b8a48e1cb9c4c714aab1ea1eea5adeffdc42a8b8
Mumo Carlos <mumo.crls@gmail.com>
2013-08-16 17:12:23 -0500

    finished (at last)

--- apps/test/views/condition_view.js
+++ apps/test/views/condition_view.js
@@ -1,11 +1,14 @@
 define(['jquery', 'underscore', 'backbone', 'hbs!test/templates/condition'],
        function($, _, Backbone, tmplCondition){
         var ConditionView = Backbone.View.extend({
-          initialize: function(){
+          initialize: function(options){
+            this.breadcrumb = options.breadcrumb;
             this.listenTo(this.model, "change", this.render);
           },
           render: function(){
-            this.$el.html(tmplCondition(this.model.attributes));
+            data = this.model.attributes;
+            data['breadcrumb'] = this.breadcrumb;
+            this.$el.html(tmplCondition(data));
             return this;
           }
         });
